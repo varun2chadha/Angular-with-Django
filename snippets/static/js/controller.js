650587ddef5ccbaa47c32f2b4e8c7d2027373fe4
Varun <varun2chadha@gmail.com>
2014-02-28 18:35:09 +0530

    i18n added, css related bugs removed

--- snippets/static/js/controller.js
+++ snippets/static/js/controller.js
@@ -4,50 +4,75 @@
  */
  
 function customerController($scope, $http) {
-     $http.get('/static/data/Rating_Plan.json').success(
-    	 function(data) {
-    		 	$scope.data = data;
-    	 }
+    $http.get('/static/data/Rating_Plan.json').success(
+        function (data) {
+            $scope.data = data;
+            $scope.trans = window.polyglot;
+        }
     );
 
 }
 
-function tabController($scope, $http){ 
+function tabController($scope, $http) {
 
-$http.get('../data/Header.json').success(
-         function(data) {
-                $scope.data = data;
-                $scope.rlconversion = function($event) {
-                    var _id=$event.currentTarget.id;
-                    if(_id=="RTL") {
-                        var angular_link =$("link[rel=stylesheet]") 
-                        $(angular_link[0]).attr({href : "/static/css/bootstrap.rtl.css"});
-                    }
-                    else if(_id=="LTR") {
-                        var angular_link =$("link[rel=stylesheet]") 
-                        $(angular_link[0]).attr({href : "/static/css/bootstrap.min.css"});
-                    }
-                }
-         }
+    $http.get('../data/Header.json').success(
+        function (data) {
+            $scope.data = data;
+            $scope.trans = window.polyglot;
+            
+        }
     );
 
-}
 
-function footersController($scope, $http){ 
-	$http.get('/static/data/Footer.json').success(
-    	 function(data) {
-    		 	$scope.data = data;
-    	 }
+   
+
+    $scope.rlconversion = function ($event) {
+        var _id = $event.currentTarget.id;
+        if (_id == "RTL") {
+            var angular_link = $("link[rel=stylesheet]")
+            $(angular_link[0]).attr({
+                href: "/static/css/bootstrap.rtl.css"
+            });
+        } else if (_id == "LTR") {
+            var angular_link = $("link[rel=stylesheet]")
+            $(angular_link[0]).attr({
+                href: "/static/css/bootstrap.min.css"
+            });
+        }
+    }
+
+    $scope.englishConversion = function ($event) {
+        _locale = 'en';
+
+        $.getJSON('/static/locales/' + 'en' + '.json', function (data) {
+            window.polyglot = new Polyglot({
+                phrases: data
+            });
+        })
+    }
+    $scope.frenchConversion = function ($event) {
+        _locale = 'de';
+
+        $.getJSON('/static/locales/' + 'de' + '.json', function (data) {
+            window.polyglot = new Polyglot({
+                phrases: data
+            });
+        })
+    }
+}
+function footersController($scope, $http) {
+    $http.get('/static/data/Footer.json').success(
+        function (data) {
+            $scope.data = data;
+            $scope.trans = window.polyglot;
+        }
     );
-
 }
-
-
-
-function ratinPlanCtrl($scope, $http){
-	  $http.get('/static/data/general-information.json').success(
-    	 function(data) {
-    		 	$scope.data = data;
-    	 }
+function ratinPlanCtrl($scope, $http) {
+    $http.get('/static/data/general-information.json').success(
+        function (data) {
+            $scope.data = data;
+            $scope.trans = window.polyglot;
+        }
     );
 }
